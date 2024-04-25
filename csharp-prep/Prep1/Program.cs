4fe25b4c48ed231704be11275f8417ca7b501852
Ourorobos <137020203+Ourorobos@users.noreply.github.com>
2024-04-24 20:43:00 -0600

    prep 1 vars hw done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string fname = "First";
+		string lname = "Last";
+		Console.WriteLine("What is your first name?");
+		fname = Console.ReadLine();
+		Console.WriteLine("What is your last name?");
+		lname = Console.ReadLine();
+		Console.WriteLine($"Your name is {lname}, {fname} {lname}");
     }
 }
