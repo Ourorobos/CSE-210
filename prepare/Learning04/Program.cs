0a83d1e4ac72c92e5a6e52f30846265cdacf71fd
Ourorobos <137020203+Ourorobos@users.noreply.github.com>
2024-05-13 20:00:39 -0600

    hw done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment math = new MathAssignment("Matt","Functions","9.75","42-111");
+        WritingAssignment writing = new WritingAssignment("Phill","New Californa Repulic","The Battle of Hoover Dam");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine("");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
